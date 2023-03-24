d63679beb59f9817776983117d9c9eb4a4d47fa3
Miloš Jovičić <milos.jovicic@planetsoft.eu>
2023-03-24 14:19:12 +0100

    init

---
+++ src/main/java/com/optaplannernqueens/OptaPlannerNQueensApplication.java
@@ -0,0 +1,13 @@
+package com.optaplannernqueens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptaPlannerNQueensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptaPlannerNQueensApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/optaplannernqueens/OptaPlannerNQueensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optaplannernqueens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptaPlannerNQueensApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
